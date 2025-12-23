a95bc9b813aa9f1913d602d2259a58b981aa353f
Diego Silveira <diego.silveira@ngi.com.br>
2025-12-22 22:24:23 -0300

    3. Gerando uma documentação e comandos para testes

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "rvbqfx",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/results",
+    overwrite: false,
+    html: true,
+    json: true,
+    timestamp: "mmddyyyy_HHMMss",
   },
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,17 @@
+Cypress.Commands.add("login", (email, password) => {
+  cy.get('[data-test="input-loginEmail"').type(email);
+  cy.get('[data-test="input-loginPassword"').type(password);
+  cy.get('[data-test="submit-button"]').click();
+});
+
+Cypress.Commands.add("cadastrar", (nameUser, email, password) => {
+  cy.get('[data-test="input-name"]').type(nameUser);
+  cy.get('[data-test="input-email"]').type(email);
+  cy.get('[data-test="input-password"]').type(password);
+  cy.get('[data-test="input-confirm-password"]').type(password);
+  cy.get('[data-test="submit-button"]').click();
+});
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
